b9cfb37e64aee88d1b7fa76ef66f2b1f75ec796d
sumit kumar <7055126157.as@gmail.com>
2022-12-11 19:14:50 +0530

    star pattern 9

---
+++ star pattern/starpattern9.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row ;
+    int coloumn;
+    char alpha;
+    for(row=1;row<=4;row++ ){
+        alpha='A';
+        for(coloumn=1;coloumn<=7;coloumn++)
+        {
+            if(coloumn<=5-row||coloumn>=3+row)
+            {
+            
+              if(coloumn<4)
+              {
+                cout<<alpha++;
+              } 
+              else{
+                cout<<alpha--;
+              }
+               
+            }
+            else{
+                cout<<" ";
+              
+                
+            }
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
